5c3fea796fb6db883d13d4a39ca7117e03b90c73
Khurram Shahzad <kshahzad@RBROSolutions.com>
2026-08-25 07:03:41 -0400

    core

--- MicroServices/MicroServices.Domain.Core/Bus/IEventBus.cs
+++ MicroServices/MicroServices.Domain.Core/Bus/IEventBus.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 namespace MicroServices.Domain.Core.Bus {
     public interface IEventBus {
         Task SendCommand<T>(T command) where T : Command;
-        void Publish<T>(T @event) where T : Event;
+        Task Publish<T>(T @event) where T : Event;
 
-        void Subscribe<T, TH>()
+        Task Subscribe<T, TH>()
             where T : Event
             where TH : IEventHandler<T>;
     }
