ce395d227f3044823dabae16565df827a6a3dda4
Khurram Shahzad <kshahzad@RBROSolutions.com>
2026-08-25 07:02:07 -0400

    ioc

--- MicroServices/MicroServices.Infrastructure.IOC/DependencyContainer.cs
+++ MicroServices/MicroServices.Infrastructure.IOC/DependencyContainer.cs
@@ -2,7 +2,6 @@
 
 using MicroServices.Banking.Application.Interfaces;
 using MicroServices.Banking.Application.Services;
-using MicroServices.Banking.Data.Context;
 using MicroServices.Banking.Data.Repository;
 using MicroServices.Banking.Domain.CommandHandlers;
 using MicroServices.Banking.Domain.Commands;
@@ -11,23 +10,30 @@ using MicroServices.Domain.Core.Bus;
 using MicroServices.Infrastructure.Bus;
 using MicroServices.Transfer.Application.Interfaces;
 using MicroServices.Transfer.Application.Services;
-using MicroServices.Transfer.Data.Context;
 using MicroServices.Transfer.Data.Repository;
 using MicroServices.Transfer.Domain.EventHandlers;
 using MicroServices.Transfer.Domain.Events;
 using MicroServices.Transfer.Domain.Interfaces;
 
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MicroServices.Infrastructure.IOC {
     public class DependencyContainer {
-        public static void RegisterServices(IServiceCollection services) {
+        public static void RegisterServices(IServiceCollection services, IConfiguration configuration) {
             //Domain Bus
-            //services.AddTransient<IEventBus, RabbitMQBus>();
+            var rabbitMqSection = configuration.GetSection("RabbitMQ");
+            var rabbitMqOptions = new RabbitMQOptions {
+                HostName = rabbitMqSection["HostName"] ?? "localhost",
+                Port = int.TryParse(rabbitMqSection["Port"], out var port) ? port : 5672,
+                UserName = rabbitMqSection["UserName"] ?? "guest",
+                Password = rabbitMqSection["Password"] ?? "guest",
+                VirtualHost = rabbitMqSection["VirtualHost"] ?? "/"
+            };
             services.AddSingleton<IEventBus, RabbitMQBus>(serviceProvider => {
                 var mediator = serviceProvider.GetService<IMediator>();
                 var scopeFactory = serviceProvider.GetRequiredService<IServiceScopeFactory>();
-                return new RabbitMQBus(mediator, scopeFactory);
+                return new RabbitMQBus(mediator, scopeFactory, rabbitMqOptions);
             });
 
             //subscription
@@ -43,8 +49,6 @@ namespace MicroServices.Infrastructure.IOC {
             //Data
             services.AddTransient<IAccountRepository, AccountRepository>();
             services.AddTransient<ITransferRepository, TransferRepository>();
-            services.AddTransient<BankingDbContext>();
-            services.AddTransient<TransferDbContext>();
         }
     }
 }
